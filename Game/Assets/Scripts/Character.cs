32fcdf50cc753e2fb1a5d265e286b9537a39fbee
DESKTOP-6UM2LMB\usang <usang6325@gmail.com>
2017-11-03 01:01:17 +0900

    game base (시작화면, 메뉴선택화면, 점프, 장애물, 체력)

---
+++ Game/Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Character : MonoBehaviour {
+	public Slider HpSlider;
+	public GameObject barriertest;
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			gameObject.GetComponent<Animator> ().Play ("Jump");
+			StartCoroutine (JumpBtn());
+		}
+    }
+
+    void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "test") {
+			HpSlider.value -= 0.08f;
+            Destroy (col.gameObject);
+		}
+	}
+
+	IEnumerator JumpBtn(){
+		GetComponent<Rigidbody2D> ().AddForce (Vector2.up * 330f);
+		yield return new WaitForSeconds(1.0f);
+	}
+
+}
