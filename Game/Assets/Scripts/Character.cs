7b5907e84bb530ec8aa09dc6b0b9d00075bed380
1G-PC\1G <dnjswl01@hanmail.net>
2017-11-23 22:28:30 +0900

    캐릭터마다 게이지가 다 차오르면 각각의 필살기 쓰게 구현(약간의 발전 필요)

--- Game/Assets/Scripts/Character.cs
+++ Game/Assets/Scripts/Character.cs
@@ -7,17 +7,31 @@ public class Character : MonoBehaviour
     public Slider HpSlider;
     public GameController GC;
 	public GameObject charac1, charac2,charac3;
+	Renderer rend;
+	public GameObject bulletPrefab,bulletPosition;
+	GameObject bull;
+	Quaternion rot;
 
 	void Start(){ //캐릭터 선택시 한 명씩 나오게
+		rot = transform.localRotation;
+		rot.eulerAngles = new Vector3 (0, 0, -90);
 		if (SelectCharacter.characterNumber == 1) {
-			charac1.transform.Translate (Vector2.right * 2.0f);
+			charac1.transform.Translate (Vector2.right * 2.3f);
+			Destroy (charac2);
+			Destroy (charac3);
 		}
 		else if (SelectCharacter.characterNumber == 2) {
 			charac2.transform.Translate (Vector2.right * 2.3f);
+			Destroy (charac1);
+			Destroy (charac3);
 		}
 		else if (SelectCharacter.characterNumber == 3) {
-			charac3.transform.Translate (Vector2.right * 2.6f);
+			charac3.transform.Translate (Vector2.right * 5.0f);
+			Destroy (charac1);
+			Destroy (charac2);
 		}
+		rend = charac3.GetComponent<Renderer> ();
+
 	}
     // Update is called once per frame
     void Update()
@@ -33,6 +47,22 @@ public class Character : MonoBehaviour
             GameController.Instance().GameOver();
             Time.timeScale = 0; // 죽었을 때 캐릭터 모습이 없어서 정지 기킨 것
         }
+
+		if (GameController.Gaugeflag == false) {  // 캐릭터 선택에 따른 필살기 
+
+			if (SelectCharacter.characterNumber == 1) {  // 슈팅
+				Shooting();
+			}
+			else if (SelectCharacter.characterNumber == 3) {
+				rend.material.color = new Color (rend.material.color.r, rend.material.color.g, rend.material.color.b, 0.4f);  // 투명화
+
+			}
+
+		} 
+		else if (GameController.Gaugeflag == true) {
+			rend.material.color = new Color (rend.material.color.r, rend.material.color.g, rend.material.color.b, 1f); // 원래 색으로 
+		}
+
     }
     void OnTriggerEnter2D(Collider2D col)
     {
@@ -45,11 +75,17 @@ public class Character : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag == "test")
-        {
-            HpSlider.value -= 0.5f;
-            Destroy(col.gameObject);
-        }
+		if (col.gameObject.tag == "test")
+		{
+
+			if(GameController.Gaugeflag == true)
+				HpSlider.value -= 0.01f;
+			else if (GameController.Gaugeflag == false && SelectCharacter.characterNumber==1) { // 1번 캐릭터 제외, 필살기를 사용중에는 hp가 닳지 않음
+				HpSlider.value -= 0.01f;
+			} 
+			Destroy (col.gameObject);
+
+		}
     }
 
     IEnumerator JumpBtn()
@@ -58,4 +94,11 @@ public class Character : MonoBehaviour
         yield return new WaitForSeconds(1.0f);
     }
 
+	void Shooting(){ // 슈팅 함수
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			bull = Instantiate(bulletPrefab, bulletPosition.transform.position, Quaternion.identity);
+			bull.transform.localRotation = rot;  // 총알그림을 90도 회전 시킴
+		} 
+		bull.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 100f);  // 총알을 오른쪽으로 나게 함
+	}
 }
--- Game/Assets/Scripts/GameController.cs
+++ Game/Assets/Scripts/GameController.cs
@@ -18,9 +18,12 @@ public class GameController : MonoBehaviour
     int score = 0;
     public int coin = 0;
     bool flag = true;
-    public GameObject enemyBarrier, enemySpike;
+	public static bool Gaugeflag = true;
+	public static GameObject enemyBarrier, enemySpike;
     bool isPlay = true;
     public Text DieText;
+	public Text descrip;
+	int count=0;
 
     public bool IsPlaying()
     {
@@ -37,9 +40,22 @@ public class GameController : MonoBehaviour
     void Update()
     {
         DieText.enabled = false;
+		descrip.enabled = false;
         if (IsPlaying() == true)
         {
             score++;  // 프레임마다 1점씩 증가
+			if (Gaugeflag == true && HpSlider.value != 0) {   //게이지 채우기
+				GaugeSlider.value += 0.0015f;  //0.0015
+				if (GaugeSlider.value == 1) {
+					Gaugeflag = false;
+				}
+			}
+			else if (Gaugeflag == false && HpSlider.value != 0) {   //게지가 꽉 찬 후 다시 줄어들기
+				GaugeSlider.value -= 0.0025f;  //0.0025
+				if (GaugeSlider.value == 0) {
+					Gaugeflag = true;
+				}
+			}
             if (score % 100 == 0)
             {  //100프레임마다 적 생성
                 if(IsPlaying() == true)
@@ -49,11 +65,17 @@ public class GameController : MonoBehaviour
                     {
                         case 1:
                             enemyBarrier = Instantiate(BarrierPrefab, enemyMakePosition.transform.position, Quaternion.identity);
-                            enemyBarrier.gameObject.tag = "test";
+							if (GameController.Gaugeflag == false && SelectCharacter.characterNumber == 2) { // 캐릭터2 필살기 장애물 속도 빠르게 
+								enemyBarrier.GetComponent<MoveObject> ().speed = 0.3f;
+							}
+							enemyBarrier.gameObject.tag = "test";
                             break;
                         case 2:
                             enemySpike = Instantiate(SpikePrefab, enemyMakePosition.transform.position, Quaternion.identity);
-                            enemySpike.gameObject.tag = "test";
+							if (GameController.Gaugeflag == false && SelectCharacter.characterNumber == 2) {  // 캐릭터2 필살기 장애물 속도 빠르게 
+								enemySpike.GetComponent<MoveObject> ().speed = 0.3f;
+							}
+							enemySpike.gameObject.tag = "test";
                             break;
                     }
                 }
@@ -65,6 +87,10 @@ public class GameController : MonoBehaviour
 
             }
             coin++;
+			if (Gaugeflag == false && SelectCharacter.characterNumber == 1 && count<100) {  // 필살기 사용 설명 나옴
+				count++;
+				descrip.enabled = true;
+			}
         }
         if (IsPlaying() == false)
         {
@@ -73,19 +99,7 @@ public class GameController : MonoBehaviour
 
         ScoreText.text = "score : " + (score) / 10;  // 점수 증가 & 표시   
         coinText.text = coin.ToString();
-        if (flag == true && HpSlider.value != 0) {   //게이지 채우기
-			GaugeSlider.value += 0.0015f;
-			if (GaugeSlider.value == 1) {
-				flag = false;
-			}
-		}
-
-		else if (flag == false && HpSlider.value != 0) {   //게지가 꽉 찬 후 다시 줄어들기
-			GaugeSlider.value -= 0.0025f;
-			if (GaugeSlider.value == 0) {
-				flag = true;
-			}
-		}
+       
     }
 
     public void GameOver()
