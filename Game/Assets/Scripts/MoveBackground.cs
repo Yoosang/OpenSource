a80659ae46c95b4ba173f8b3baf81e7df1af0393
1G-PC\1G <dnjswl01@hanmail.net>
2017-11-24 23:54:03 +0900

    배경 움직임

--- Game/Assets/Scripts/MoveBackground.cs
+++ Game/Assets/Scripts/MoveBackground.cs
@@ -2,26 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class MoveBackground : MonoBehaviour {
 
-    public float speed; //이동 속도
+	public const float scrollSpeed = 0.03f; 
+	//스크롤할 속도를 상수로 지정해 줍니다.
+	private Material thisMaterial;
+	//Quad의 Material 데이터를 받아올 객체를 선언합니다
+	// Use this for initialization
+	void Start () {
+		//객체가 생성될때 최초 1회만 호출 되는 함수 입니다.
+		thisMaterial = GetComponent<Renderer>().material; 
+		//현재 객체의 Component들을 참조해 Renderer라는 컴포넌트의 Material정보를 받아옵니다.
 
-    void Start()
-    {
+	}
 
-    }
+	// Update is called once per frame
+	void Update () {
+		Vector2 newOffset = thisMaterial.mainTextureOffset;
+		// 새롭게 지정해줄 OffSet 객체를 선언합니다.
+		newOffset.Set(newOffset.x + (scrollSpeed * Time.deltaTime),0 );
+		// Y부분에 현재 y값에 속도에 프레임 보정을 해서 더해줍니다.
+		thisMaterial.mainTextureOffset = newOffset;
+		//그리고 최종적으로 Offset값을 지정해줍니다.
 
-    // Update is called once per frame
-    void Update()
-    {
-        //왼쪽방향으로 speed만큼 이동하게됩니다.
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-
-        //x좌표가 -12.8보다 작아지게 되면...
-        if (transform.position.x < -12.8f)
-        {
-            //위치를 0, 0 으로 옮겨줍니다.
-            transform.position = new Vector2(0, 0);
-        }
-    }
+	}
 }
